49c9c01598f934393edef15435ea3b00a40687f9
donramones <agusrivarolaa00@gmail.com>
2025-04-03 22:40:32 -0300

    ejercicio_else

---
+++ 03 Estructuras Condicionales/Ejercicio_ELif/ejercicio_2_elif.py
@@ -0,0 +1,9 @@
+a = int(input("coloca un numero entero A: "))
+b = int(input("coloca un numero entero B: "))
+
+if b!= 0:
+    division = a/b
+    print("El resultado de division es",division)
+else:
+    print("No se puede dividir por cero")
+print("Chau!")

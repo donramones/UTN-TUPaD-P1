abced3f69c920df8eb60a64e3c8a97b632ed6df5
donramones <agusrivarolaa00@gmail.com>
2025-04-03 22:19:11 -0300

    tes_1_if

--- 03 Estructuras Condicionales/Ejercicio_ELif/ejercicio_elif.py
+++ 03 Estructuras Condicionales/Ejercicio_ELif/ejercicio_elif.py
@@ -1,3 +1,7 @@
+a = int(input("coloca un numero entero A: "))
+b = int(input("coloca un numero entero B: "))
 
-
-
+if b!= 0:
+    division = a/b
+    print("El resultado de division es",division)
+print("Chau!")

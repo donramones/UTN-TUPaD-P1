78ae76310e550baec0da45d13c95cf5ea897f547
donramones <agusrivarolaa00@gmail.com>
2025-04-03 22:05:03 -0300

    ejerciocio elif

--- 03 Estructuras Condicionales/ejercicio_3.py
+++ 03 Estructuras Condicionales/ejercicio_3.py
@@ -3,6 +3,7 @@ EDAD_MINIMA = 21
 edad= int(input("Ingrese su edad: "))
 categoria= input("Ingrese su categoria (A, B, C, D, E, F, G): ")
 
+#se agrega () a (categoria == "D" or categoria == "d"): , para evaluar dos resultados
 if edad >= EDAD_MINIMA and (categoria == "D" or categoria == "d"):
     print("Puede conducir ambulancias")
 else:

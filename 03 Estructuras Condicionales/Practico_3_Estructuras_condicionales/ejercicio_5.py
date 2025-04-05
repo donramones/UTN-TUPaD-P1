38f496c7f933f07015ba612234b47b1ec2e5c1f3
donramones <agusrivarolaa00@gmail.com>
2025-04-04 23:24:53 -0300

    ejercicio_5_ok

---
+++ 03 Estructuras Condicionales/Practico_3_Estructuras_condicionales/ejercicio_5.py
@@ -0,0 +1,5 @@
+contraseña= input("Ingrese una contraseña de 8 a 14 caracteres: ")
+if len (contraseña) >=8  <= 14:
+    print( "Ha ingresado una contraseña correcta")
+else:
+    print("Por favor, ingrese una contraseña de entre 8 y 14 caracteres")

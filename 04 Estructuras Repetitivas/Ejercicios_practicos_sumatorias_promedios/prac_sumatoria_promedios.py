efbca20fd9a1af2a9d99da187a906795bbd4b99d
donramones <agusrivarolaa00@gmail.com>
2025-04-18 19:52:11 -0300

    sumatoria_promedio

---
+++ 04 Estructuras Repetitivas/Ejercicios_practicos_sumatorias_promedios/prac_sumatoria_promedios.py
@@ -0,0 +1,12 @@
+
+#sumatoria de numeros y mostrarlos por panatalla
+
+cant_numeros = 5
+sumatoria = 0
+for cont in range (1,cant_numeros+1):
+    print("ingrese numero",cont)
+    num = int(input())
+    sumatoria = sumatoria + num
+print("La sumatoria de los valores es: ",sumatoria)
+#agrego una linea para calcular el promedio,ya cuento con las veriables sumatoria/cant_numeros
+print("El promedio es: ",(sumatoria/cant_numeros))

724be192b079e10a83ddbd81f85cd88da8955b47
donramones <agusrivarolaa00@gmail.com>
2025-04-21 18:32:05 -0300

    ciclos

---
+++ 04 Estructuras Repetitivas/Ejercicios_ciclos_por_contador/prac_ciclos_por_contador.py
@@ -0,0 +1,17 @@
+#print("Hola")
+
+#cont = 1
+#while cont <= 5:
+    #print(cont,"ciclos")
+    #cont = cont +1 
+#print("Chau")
+
+
+print("Hola")
+
+cont = 5
+while cont >= 1:
+    print(cont,"ciclos")
+    cont = cont - 1 
+print("Chau")
+
--- 04 Estructuras Repetitivas/Ejercicios_practicos_sumatorias_promedios/prac_sumatoria_promedios.py
+++ 04 Estructuras Repetitivas/Ejercicios_practicos_sumatorias_promedios/prac_sumatoria_promedios.py
@@ -6,7 +6,7 @@ sumatoria = 0
 for cont in range (1,cant_numeros+1):
     print("ingrese numero",cont)
     num = int(input())
-    sumatoria = sumatoria + num
+    sumatoria = sumatoria + num #sumatoria += num ; cont += 1
 print("La sumatoria de los valores es: ",sumatoria)
 #agrego una linea para calcular el promedio,ya cuento con las veriables sumatoria/cant_numeros
 print("El promedio es: ",(sumatoria/cant_numeros))

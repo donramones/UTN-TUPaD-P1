847765e0a936862cc2a29fc9787d6a3ea02e0b5d
donramones <agusrivarolaa00@gmail.com>
2025-04-22 20:24:52 -0300

    practico_1

---
+++ 04 Estructuras Repetitivas/ejercicio_test/prueba_1.py
@@ -0,0 +1,25 @@
+#desarrollar programa que permita ingresar un numero entero positivo. La computadora debe mostrar  la sucesion  de numeros  desde el numero ingresado hasta el cero
+#en una sola linea
+#numero = int(input("Ingrese un numero positivo: "))
+
+#if numero >0:
+    #cont = numero
+    #while cont >= 0:
+        #print(cont)
+        #cont = cont -2
+
+#else:
+    #print("El numero deberia ser positivo")
+
+numero = int(input("Ingrese un numero positivo: "))
+
+if numero >0: 
+    if  numero % 2 != 0: #para verificar numeros impares , utilizo % que me devuelve el resto
+        numero = numero -1
+    cont = numero
+    while cont >= 0:
+        print(str(cont) + " ", end="") #ens="" salto de linea , str funcion que permite convertir a cadena cualquier numero
+        cont = cont -2
+
+else:
+    print("El numero deberia ser positivo")

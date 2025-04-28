ee9a5492da70ed6941c335fe320642b0b8fc1dc2
donramones <agusrivarolaa00@gmail.com>
2025-04-27 21:40:33 -0300

    test

--- 04 Estructuras Repetitivas/ejercicio_test/test.py
+++ 04 Estructuras Repetitivas/ejercicio_test/test.py
@@ -1,6 +1,11 @@
+#multiplicacion=0
+#for numero in range (1,6):
+    #multiplicacion *= numero
+#print(f"El resultado final es: {multiplicacion}")
 
-
-for i in range (2,10,2):
-   print(i)
-
-
+x= 0
+for i in range(1,6):
+    x += i
+    if x >10:
+        break
+print(x)

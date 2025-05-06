06c4097dcd284db51e84a01fd6eca9b87cb96ad3
donramones <agusrivarolaa00@gmail.com>
2025-05-05 22:53:59 -0300

    ejercicio_ok

---
+++ 005 Listas/ejercicio_4.py
@@ -0,0 +1,5 @@
+#Reemplazar el segundo y último valor de la lista “animales” con las palabras “loro” y “oso”, respectivamente. Imprimir la lista resultante por pantalla.
+#animales = ["perro", "gato", "conejo", "pez"]
+animales = ["perro", "gato", "conejo", "pez"]
+animales [2:4] = "loro","oso"
+print(animales)

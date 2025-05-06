5d7b976ef5ce646541515b7b4fb4f9cc6e5cb366
donramones <agusrivarolaa00@gmail.com>
2025-05-05 22:22:07 -0300

    ejercicio_ok

---
+++ 005 Listas/Ejercicio_2.py
@@ -0,0 +1,7 @@
+#Crear una lista con cinco elementos (colocar los elementos que más te gusten) y mostrar el penúltimo.
+#¡Puedes hacerlo como se muestra en los videos o bien investigar cómo funciona el indexing con números negativos!
+
+lista_mix=["CAB",1,2,3,"UTN",99]
+penultimo_lista=lista_mix[-2]
+print(penultimo_lista)
+

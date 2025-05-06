855c8dbef74f6bf0cac5c2e9c1b806edadcdd625
donramones <agusrivarolaa00@gmail.com>
2025-05-05 23:25:31 -0300

    ejercicio_ok

---
+++ 005 Listas/Ejercicio_7.py
@@ -0,0 +1,5 @@
+#Reemplazar los dos valores centrales (índices 1 y 2) de la lista “autos” por dos nuevos valores cualesquiera
+#autos = ["sedan", "polo", "suran", "gol"]
+autos = ["sedan", "polo", "suran", "gol"]
+autos [1:3]="sandero" , "Torino"
+print(autos)

a9b64e22be87d91dd3eecfaa102c1ade2e9335cc
donramones <agusrivarolaa00@gmail.com>
2025-05-05 23:17:08 -0300

    ejercicio_ok

---
+++ 005 Listas/Ejercicio_6.py
@@ -0,0 +1,4 @@
+#Crear una lista con números del 10 al 30 (incluído), haciendo saltos de 5 en 5 y mostrar por pantalla los dos primeros.
+lista_numeros = list(range (10,31,5))
+dos_primeros=lista_numeros[0:2]
+print(dos_primeros)

80446ea1d637919f13d98e3bf504077efd678044
donramones <agusrivarolaa00@gmail.com>
2025-05-30 21:13:27 -0300

    fibo

---
+++ 006 Recursividad/2_ejercicio.py
@@ -0,0 +1,10 @@
+#Crea una función recursiva que calcule el valor de la serie de Fibonacci en la posición indicada
+#Posteriormente, muestra la serie completa hasta la posición que el usuario especifique.
+
+def fibonnaci (n):
+    if == 0 or n == 1:
+        return n
+    else:
+        return fibonnaci(n-1) + fibonnaci (n-2)
+    
+print(fibonnaci(6))
